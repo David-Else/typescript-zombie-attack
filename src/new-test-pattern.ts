e5f0394ad819ae803688989f8f4fb6ee2c1aa58b
David <12832280+David-Else@users.noreply.github.com>
2020-02-20 09:25:13 +0000

    Add comments

--- src/new-test-pattern.ts
+++ src/new-test-pattern.ts
@@ -18,6 +18,54 @@ class Entity {
   }
 }
 
+/**
+ * ============================================================================
+ * Graphic components
+ * ============================================================================
+ */
+
+class BitmapRenderable implements Delegate {
+  constructor(
+    private ctx: CanvasRenderingContext2D,
+    private image: HTMLImageElement, // Needs to be async
+    private readonly positionable: Positionable
+  ) {}
+  public update(): void {
+    const { position } = this.positionable;
+    this.ctx.drawImage(this.image, position[0], position[1]);
+  }
+}
+
+class RectangleRenderable implements Delegate {
+  constructor(
+    private ctx: CanvasRenderingContext2D,
+    private readonly fill: string,
+    private readonly positionable: Positionable
+  ) {}
+  public update(): void {
+    const { position, width, height, rotation } = this.positionable;
+    // first save the untranslated/unrotated context
+    this.ctx.save();
+    this.ctx.beginPath();
+    // move the rotation point to the center of the rect
+    this.ctx.translate(position[0] + width / 2, position[1] + height / 2);
+    // rotate the rect
+    this.ctx.rotate(rotation * (Math.PI / 180));
+    // draw the rect on the transformed context
+    // Note: after transforming [0,0] is visually [x,y]
+    //       so the rect needs to be offset accordingly when drawn
+    this.ctx.fillStyle = this.fill;
+    this.ctx.fillRect(width / -2, height / -2, width, height);
+    // restore the context to its untranslated/unrotated state
+    this.ctx.restore();
+  }
+}
+
+/**
+ * ============================================================================
+ * Physics components
+ * ============================================================================
+ */
 class DirectTowardsable implements Delegate {
   constructor(
     private targetPosition: Vector2,
@@ -63,43 +111,11 @@ class Positionable {
   }
 }
 
-class BitmapRenderable implements Delegate {
-  constructor(
-    private ctx: CanvasRenderingContext2D,
-    private image: HTMLImageElement, // Needs to be async
-    private readonly positionable: Positionable
-  ) {}
-  public update(): void {
-    const { position } = this.positionable;
-    this.ctx.drawImage(this.image, position[0], position[1]);
-  }
-}
-
-class RectangleRenderable implements Delegate {
-  constructor(
-    private ctx: CanvasRenderingContext2D,
-    private readonly fill: string,
-    private readonly positionable: Positionable
-  ) {}
-  public update(): void {
-    const { position, width, height, rotation } = this.positionable;
-    // first save the untranslated/unrotated context
-    this.ctx.save();
-    this.ctx.beginPath();
-    // move the rotation point to the center of the rect
-    this.ctx.translate(position[0] + width / 2, position[1] + height / 2);
-    // rotate the rect
-    this.ctx.rotate(rotation * (Math.PI / 180));
-    // draw the rect on the transformed context
-    // Note: after transforming [0,0] is visually [x,y]
-    //       so the rect needs to be offset accordingly when drawn
-    this.ctx.fillStyle = this.fill;
-    this.ctx.fillRect(width / -2, height / -2, width, height);
-    // restore the context to its untranslated/unrotated state
-    this.ctx.restore();
-  }
-}
-
+/**
+ * ============================================================================
+ * Input components
+ * ============================================================================
+ */
 class KeyboardInputable implements Delegate {
   private firePaused = false;
   private keysPressed = {
@@ -163,6 +179,11 @@ class KeyboardInputable implements Delegate {
   };
 }
 
+/**
+ * ============================================================================
+ * Entity constructors
+ * ============================================================================
+ */
 export const createHero = (
   ctx: CanvasRenderingContext2D,
   position: Vector2
